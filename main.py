8fef84f27ac151d1558f376a4858d88fa37305c6
Khuselo Nditha <khuselonditha@gmail.com>
2024-12-20 13:19:55 +0200

    After checking if the corect keys are being returned.

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ url += "?q=launguage:python+sort:stars+stars:>1000"
 headers = {"Accept": "application/vnd.github.v3+json"}
 response = requests.get(url, headers=headers)
 print(f"Status code: {response.status_code}")
+
+# Convert the response to a dictionary
+response_dict = response.json()
+
+# Print the response
+print(response_dict.keys())
