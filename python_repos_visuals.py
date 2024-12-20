e662b4519ddafbfe75f0d527feaf50571ecf0a39
Khuselo Nditha <khuselonditha@gmail.com>
2024-12-20 17:30:22 +0200

    After storing the reponames and stars in their own lists.

--- python_repos_visuals.py
+++ python_repos_visuals.py
@@ -13,3 +13,10 @@ print(f"Status code: {response.status_code}")
 # Process the results
 response_dict = response.json()
 print(f"Complete results: {not response_dict['incomplete_results']}")
+
+# Process repository information
+repo_dict = response_dict["items"]
+repo_names, stars = [], []      # Lists to store repository names and stars
+for repo in repo_dict:
+    repo_names.append(repo["name"])
+    stars.append(repo["stargazers_count"])
