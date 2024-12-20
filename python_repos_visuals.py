894fbbe787d3ddb1f893d9d57b037e4e188c8c94
Khuselo Nditha <khuselonditha@gmail.com>
2024-12-20 18:30:46 +0200

    After saving the generated html for offline use.

--- python_repos_visuals.py
+++ python_repos_visuals.py
@@ -1,6 +1,7 @@
 # imports
 import requests
 import plotly.express as px
+from plotly import offline
 
 # Make an API call and check the response
 url = "https://api.github.com/search/repositories"
@@ -20,3 +21,8 @@ repo_names, stars = [], []      # Lists to store repository names and stars
 for repo in repo_dict:
     repo_names.append(repo["name"])
     stars.append(repo["stargazers_count"])
+
+# Make visualization
+fig = px.bar(x=repo_names, y=stars, title="Most-Starred Python Projects on GitHub")
+fig.update_layout(xaxis_title="Repositories", yaxis_title="Stars")
+offline.plot(fig, filename='charts/most_starred_python_repos.html')
