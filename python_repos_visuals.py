4def2ff9e6c9f67f952a05ff3c492055fed0aa0a
Khuselo Nditha <khuselonditha@gmail.com>
2024-12-20 16:44:47 +0200

    After test if the API is configured correctly.

--- python_repos_visuals.py
+++ python_repos_visuals.py
@@ -1,3 +1,11 @@
 # imports
 import requests
 import plotly.express as px
+
+# Make an API call and check the response
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>1000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+response = requests.get(url, headers=headers)
+print(f"Status code: {response.status_code}")
