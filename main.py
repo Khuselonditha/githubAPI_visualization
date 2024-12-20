bc4018eba33ba48d8fb23b7c49eb223137ce26bd
Khuselo Nditha <khuselonditha@gmail.com>
2024-12-20 13:37:20 +0200

    After printing and returning the total number of repos.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 # Make an API call and check the response
 url = "https://api.github.com/search/repositories"
-url += "?q=launguage:python+sort:stars+stars:>1000"
+url += "?q=language:python+sort:stars+stars:>1000"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 response = requests.get(url, headers=headers)
@@ -14,3 +14,7 @@ response_dict = response.json()
 
 # Print the response
 print(response_dict.keys())
+
+# Print the total number of repositories
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
